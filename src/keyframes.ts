269238f16fd70da35b87b33a373c1db346705474
Beka Westberg <bekawestberg@gmail.com>
2025-08-08 22:22:05 -0700

    fixup implementation to actually work

--- src/keyframes.ts
+++ src/keyframes.ts
@@ -1,7 +1,11 @@
 export function ensureKeyframes() {
   if (!(window as any).__rno_kf_s) {
-    const style = (window as any).__rno_kf_s = document.createElement('style');
-    style.textContent = `@keyframes rough-notation-dash { to { stroke-dashoffset: 0; } }`;
+    const style = ((window as any).__rno_kf_s =
+      document.createElement("style"));
+    style.textContent = `
+      @keyframes rough-notation-dash { to { stroke-dashoffset: 0; } }
+      @keyframes rough-notation-dash-reverse { to { stroke-dashoffset: var(--path-length); } }
+    `;
     document.head.appendChild(style);
   }
 }
